f9e2a2757877952b4b12207b2c28fe7faa6e45f2
YurasKamaz <tyertyn@gmail.com>
2022-08-10 06:46:25 +0300

    Updates

---
+++ precache-manifest.4beb7381633ed62f65405e386941adfa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "df1cfe4be57ed67ebe8e",
+    "url": "/DimasGovnas/static/css/main.bb6a7b05.chunk.css"
+  },
+  {
+    "revision": "df1cfe4be57ed67ebe8e",
+    "url": "/DimasGovnas/static/js/main.df1cfe4b.chunk.js"
+  },
+  {
+    "revision": "76c0df16066e3d03aa4b",
+    "url": "/DimasGovnas/static/js/1.76c0df16.chunk.js"
+  },
+  {
+    "revision": "b8190b1db2981cb97843",
+    "url": "/DimasGovnas/static/js/runtime~main.b8190b1d.js"
+  },
+  {
+    "revision": "06fe9d9a673b4fde95fbd958c37881b3",
+    "url": "/DimasGovnas/index.html"
+  }
+];
